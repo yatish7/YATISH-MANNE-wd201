5d4c2ec489cbb4a790b5f5f88c278e852ebfe54a
yatish7 <yatish.manne09@gmail.com>
2023-12-20 19:37:04 +0530

    Final

--- HelloWorld/index.js
+++ HelloWorld/index.js
@@ -1,11 +1,1 @@
-const express = require('express');
-const app = express();
-
-app.get('/',(req,res)=>{
-    console.log("Hello GitHub!")
-    res.write("Hello GitHub!");
-})
-
-app.listen(8000,()=>{
-    console.log("Server running on port 8000")
-})
+console.log("Hello GitHub!")
